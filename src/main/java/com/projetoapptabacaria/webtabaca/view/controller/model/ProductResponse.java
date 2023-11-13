fd42640add60aaef1d102a9b34c08271ae72f037
Raphael moura <raphaelmvb2@gmail.com>
2023-11-13 13:52:35 -0300

    Criação da classe ProductResponse

---
+++ src/main/java/com/projetoapptabacaria/webtabaca/view/controller/model/ProductResponse.java
@@ -0,0 +1,51 @@
+package com.projetoapptabacaria.webtabaca.view.controller.model;
+
+public class ProductResponse {//resposta do Product Request, o que vou devolver
+
+   // private Long id;
+    private String name;
+    private Long quantity;
+    private Double value;
+    private String description;
+
+    /*public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+*/
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+
