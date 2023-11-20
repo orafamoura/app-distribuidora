956afdae16ccbfde0320bfb7bca663ee35258e76
Raphael moura <raphaelmvb2@gmail.com>
2023-11-20 18:15:39 -0300

    Criação da classe UserRepository

---
+++ src/main/java/com/projetoapptabacaria/webtabaca/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.projetoapptabacaria.webtabaca.repositories;
+
+import com.projetoapptabacaria.webtabaca.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
