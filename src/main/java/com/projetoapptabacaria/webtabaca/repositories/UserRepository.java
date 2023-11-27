5ee93480dc8109e1beb4de6e8df3de587e07d337
Raphael moura <raphaelmvb2@gmail.com>
2023-11-27 19:50:06 -0300

    Criacao de metodo findByLogin e atualizacao de imports

--- src/main/java/com/projetoapptabacaria/webtabaca/repositories/UserRepository.java
+++ src/main/java/com/projetoapptabacaria/webtabaca/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.projetoapptabacaria.webtabaca.repositories;
 
-import com.projetoapptabacaria.webtabaca.model.User;
+import com.projetoapptabacaria.webtabaca.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
+    UserDetails findByLogin(String login); // retorna um userdetails pois vai ser usado pelo security depois
 }
